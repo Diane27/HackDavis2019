a53b8140fa7c3831435d15571e8969732945eda6
Jesse Smick <janka102@gmail.com>
2019-02-10 07:07:59 -0800

    start on qrcode

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { AsyncStorage } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button, View } from 'react-native-elements';
 import HeaderTitle from '../components/HeaderTitle.js';
 
 import * as firebase from 'firebase';
+import QRCode from 'react-native-qrcode';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
@@ -16,9 +17,19 @@ export default class SettingsScreen extends React.Component {
     this.props.navigation.navigate('Auth');
   }
 
-  render() {
+  async render() {
+    let qrcode = null
+    let uid = await AsyncStorage.getItem('userId');
+
+    if (uid) {
+      qrcode = <QRCode value={uid} size={200} ></QRCode>
+    }
+
     return (
-      <Button title="Sign Out" onPress={this._signOut} />
+      <View>
+        {qrcode && qrcode}
+        <Button title="Sign Out" onPress={this._signOut} />
+      </View>
     );
   }
 }
