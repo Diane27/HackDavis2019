2b39713540ae08f00dba6f7b9c8d047ce07bcf1e
Jesse Smick <janka102@gmail.com>
2019-02-09 23:01:25 -0800

    Add more cards

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,7 +8,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import { WebBrowser } from 'expo';
+import moment from 'moment';
 
 import styles from '../styles/home.scss';
 import HomeCard from '../components/HomeCard.js';
@@ -23,7 +23,17 @@ export default class HomeScreen extends React.Component {
       {
         super: 'hello',
         title: 'John Smith',
-        subText: 'Age: 71'
+        subText: ['Age: 71', '\n', 'Caretaker: Jane Doe']
+      },
+      {
+        super: 'Today is',
+        title: [moment().format('dddd h:mm A'), '\n', moment().format('D MMMM YYYY')],
+        subText: ''
+      },
+      {
+        super: 'A reminder for you:',
+        title: 'Your caretaker will take you to the doctor at 1:00PM',
+        subText: ''
       }
     ]
   };
@@ -37,37 +47,4 @@ export default class HomeScreen extends React.Component {
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
