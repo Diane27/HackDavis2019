4fb4e7b49b361030768651de7e42a50ab824c5d2
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-02-10 05:46:21 -0800

    Add pages and routes for patient

--- screens/patient/ChatScreen.js
+++ screens/patient/ChatScreen.js
@@ -9,14 +9,12 @@ import {
   View,
 } from 'react-native';
 import { Icon } from 'expo';
-import moment from 'moment';
 import * as firebase from 'firebase';
 
 import styles from '../../styles/home.scss';
-import HomeCard from '../../components/HomeCard.js';
 import HeaderTitle from '../../components/HeaderTitle.js';
 
-export default class HomeScreen extends React.Component {
+export default class ChatScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
       headerTitle: <HeaderTitle/>,
@@ -33,43 +31,17 @@ export default class HomeScreen extends React.Component {
     const userId = await AsyncStorage.getItem('userId');
     const result = await firebase.firestore().collection('users').doc(userId).get();
     const name = result.get('name');
-    const nameCard = this.state.cards[0];
-    nameCard.message = name;
-
-    this.setState({
-      cards: [nameCard, ...this.state.cards.slice(1)]
-    });
   }
 
   _goToSettings = () => {
     this.props.navigation.navigate('Settings');
   }
 
-  state = {
-    cards: [
-      {
-        title: 'Hello,',
-        message: '',
-        subText: ''
-      },
-      {
-        title: 'Today is',
-        message: [moment().format('dddd h:mm A'), '\n', moment().format('D MMMM YYYY')],
-        subText: ''
-      },
-      {
-        title: 'A reminder for you:',
-        message: 'Your caretaker will take you to the doctor at 1:00PM',
-        subText: ''
-      }
-    ]
-  };
-
   render() {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          {this.state.cards.map((card, i) => <HomeCard card={card} key={i}></HomeCard>)}
+          <Text>Chat</Text>
         </ScrollView>
       </View>
     );
