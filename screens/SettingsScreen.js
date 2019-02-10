55f16b15f285dbd28c9bb5d2628cef3db3c5bf03
Jesse Smick <janka102@gmail.com>
2019-02-10 09:42:19 -0800

    Merge master

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { AsyncStorage } from 'react-native';
-import { Button, View } from 'react-native-elements';
+import {
+  AsyncStorage,
+  TouchableOpacity,
+  View,
+  ScrollView
+} from 'react-native';
+import { Button, Image } from 'react-native-elements';
+import { Icon } from 'expo';
 import HeaderTitle from '../components/HeaderTitle.js';
 import styles from '../styles/home.scss';
 
 import * as firebase from 'firebase';
-import QRCode from 'react-native-qrcode';
+import * as QRCode from 'qrcode';
+
+const generateQR = async text => {
+  try {
+    return await QRCode.toDataURL(text);
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -45,17 +59,19 @@ export default class SettingsScreen extends React.Component {
   }
 
   async render() {
-    let qrcode = null
+    let qrcode = []
     let uid = await AsyncStorage.getItem('userId');
 
     if (uid) {
-      qrcode = <QRCode value={uid} size={200} ></QRCode>
+      // console.log(await generateQR(uid));
+      // qrcode.push(<Image source={{ uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==' }} style={{width: 400, height: 400}} key="qr"/>);
     }
 
     return (
-      <View>
-        {qrcode && qrcode}
-        <Button title="Sign Out" onPress={this._signOut} />
+      <View style={styles.container}>
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <Button title="Sign Out" onPress={this._signOut} />
+        </ScrollView>
       </View>
     );
   }
