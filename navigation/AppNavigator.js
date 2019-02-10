9a220a8e18f9d00cb7b32e44224d741a72c3d993
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-02-09 21:20:49 -0800

    Setup navigation

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import AuthNavigator from './AuthNavigator';
 
-export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-}));
+import AuthLoadingScreen from '../screens/AuthLoadingScreen';
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    Auth: AuthNavigator,
+    Main: MainTabNavigator
+  },
+  {
+    initialRouteName: 'AuthLoading'
+  }
+));
